e1d79bfdf9ec1b14ea0144a1e91b82d1bef1364f
SuNgu <cliffland.jasungu@gmail.com>
2024-08-04 17:07:44 +0300

    initialized the array of quotes

---
+++ dom-manipulation/scripts.js
@@ -0,0 +1,6 @@
+// initialize the array of quotes
+let quotes = [
+    { text: "The only limit to our realization of tomorrow is our doubts of today.", category: "Motivation" },
+    { text: "Life is what happens when you're busy making other plans.", category: "Life" },
+    { text: "The purpose of our lives is to be happy.", category: "Happiness" }
+];
