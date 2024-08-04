13fcb071e5c2dd06089118bc319800441cca0b34
SuNgu <cliffland.jasungu@gmail.com>
2024-08-04 17:10:39 +0300

    added event listener for sharing a new random quote

--- dom-manipulation/scripts.js
+++ dom-manipulation/scripts.js
@@ -4,3 +4,11 @@ let quotes = [
     { text: "Life is what happens when you're busy making other plans.", category: "Life" },
     { text: "The purpose of our lives is to be happy.", category: "Happiness" }
 ];
+
+// Function to show a random quote
+function showRandomQuote() {
+    const quoteDisplay = document.getElementById('quoteDisplay');
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+    quoteDisplay.textContent = `"${randomQuote.text}" - Category: ${randomQuote.category}`;
+}
